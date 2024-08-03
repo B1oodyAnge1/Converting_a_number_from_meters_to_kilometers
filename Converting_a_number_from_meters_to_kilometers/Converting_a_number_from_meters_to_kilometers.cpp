da11d4bcab077b9d244df564940b910584f02b77
B1oodyAnge1 <romsm@bk.ru>
2024-08-03 17:10:08 +0300

    The task is completed

--- Converting_a_number_from_meters_to_kilometers/Converting_a_number_from_meters_to_kilometers.cpp
+++ Converting_a_number_from_meters_to_kilometers/Converting_a_number_from_meters_to_kilometers.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+    float m,a = 1000;
+    std::cout << "Перевод метров в километры\n";
+    std::cout << "Введите метры: \n";
+    std::cin >> m;
+    std::cout << m << " метров это " << m / a << " километров.\n";
+    return 0;
 }
